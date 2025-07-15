432368b206e50d5f349e296c355ff477b05b1fb6
EREoreo <error323269@gmail.com>
2025-07-16 02:09:04 +0500

    ТРЕТИй

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,8 @@ export default defineConfig({
     },
   },
   server: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5173,
     proxy: {
       // Проксируем все запросы /api/* на ваш Express‑сервер на порту 3000
       '/api': {
